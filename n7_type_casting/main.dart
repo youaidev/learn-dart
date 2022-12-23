657e3af56f79e6656ca53cdd8810e897a8d4db6e
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-23 14:26:43 +0100

    Create main.dart

---
+++ n7_type_casting/main.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(){
+
+  // string to int
+  print("Enter number :");
+  double d = double.parse(stdin.readLineSync()!);
+  print("d = $d");
+
+  // string to int
+  print("Enter number :");
+  int i = int.parse(stdin.readLineSync()!);
+  print("i = $i");
+  
+}
