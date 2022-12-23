a68f72fd3ef7422dc894ef5dbebcfc20304d2651
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-23 14:26:41 +0100

    Create main.dart

---
+++ n6_string/main.dart
@@ -0,0 +1,13 @@
+
+void main(){
+
+  print('this\'s');
+  print("this's");
+  print("this's \ samy'");
+  print(r"this's \ samy'");
+  print("Str".length);
+  print("Str".toLowerCase());
+  print("Str".toUpperCase());
+
+
+}
