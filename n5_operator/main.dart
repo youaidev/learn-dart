f9936d14915f9d9d0c0d73d9949f3c10a6b6277d
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-23 10:42:13 +0100

    Create main.dart

---
+++ n5_operator/main.dart
@@ -0,0 +1,28 @@
+
+void main() {
+
+  // Multiply
+  int a = 13;
+  int b = 2;
+  int mult = a * b;
+  print('mult = $a * $b = $mult');
+
+  // Division
+  int int_div = a ~/ b;
+  print('int_div = $a ~/ $b = $int_div');
+  double double_div = a / b;
+  print('double_div = $a / $b = $double_div');
+
+  // Addition
+  int add = a + b;
+  print('add = $a + $b = $add');
+
+  // Subtraction
+  int sub = a - b;
+  print('sub = $a - $b = $sub');
+
+  // Modulus
+  int mod = a % b;
+  print('mod = $a % $b = $mod');
+
+}
