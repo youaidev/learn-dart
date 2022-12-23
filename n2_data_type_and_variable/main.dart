4837fa10bdcc984274329f18d16359d847d3d4b3
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-23 10:15:00 +0100

    Create main.dart

---
+++ n2_data_type_and_variable/main.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  String name;
+  name = 'Jak';
+  print('name = $name');
+
+  int age;
+  age = 5;
+  print('name = $age years');
+
+  double speed;
+  speed = 15.33;
+  print('name = $speed km/h');
+
+  
+}
