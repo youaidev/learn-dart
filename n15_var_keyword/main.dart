a3361027435b3cf9793dbf2102272b2758c5e111
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-25 10:43:49 +0100

    Create main.dart

---
+++ n15_var_keyword/main.dart
@@ -0,0 +1,25 @@
+
+
+void main(){
+
+  // var 
+  var s = 'str';
+  s = 'text';
+
+  var i = 10;
+  i = 20;
+
+  var d = 1.0;
+  d = 2.0;
+
+  var b = true;
+  b = false;
+
+  // var dynamic by default
+  var x;
+  x = 'str';
+  x = 10;
+  x = 1.0;
+  x = true;
+
+}
