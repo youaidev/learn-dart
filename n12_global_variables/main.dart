9abc067e98ccd4a6c0ed7a1cac893fdc681a1c85
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-25 10:43:39 +0100

    Create main.dart

---
+++ n12_global_variables/main.dart
@@ -0,0 +1,16 @@
+
+
+
+int var_global = 0;
+
+void main(List<String> args) {
+  
+  int var_in_main = 1;
+
+  if (var_in_main == 1) {
+    print("var_in_main = $var_in_main");
+  }
+  
+  print('var_global = $var_global');
+
+}
