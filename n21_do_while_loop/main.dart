41cbfa4497b6e40f71c7579f834241a5985efd3f
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-27 11:59:55 +0100

    Create main.dart

---
+++ n21_do_while_loop/main.dart
@@ -0,0 +1,19 @@
+// loops : for, while, do while, forEach
+
+void main(){
+
+  // do{} while(condition)
+  // do than check
+
+  
+  int start = 0;
+  int end = 5;
+  do{
+    print('$start < $end');
+    start = start + 1;
+  } while(false);
+  
+
+ 
+
+}
