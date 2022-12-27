76a15c29e8a38f8ce19670427d100a87e7861f96
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2022-12-27 11:59:43 +0100

    Update main.dart

---
+++ n16_list/main.dart
@@ -0,0 +1,18 @@
+
+// collection : list, set, map
+
+void main(){
+
+  List <String> ls = ['text1','text2'];
+  List <int> li = [11, 22];
+  List <double> ld = [1.1, 2.2];
+  List <dynamic> ldy = ['text', 11, 1.1];
+
+  // value = variable[index]
+  print('ldy[0] = ${ldy[0]}');
+  print('ldy[1] = ${ldy[1]}');
+  print('ldy[2] = ${ldy[2]}');
+
+
+
+}
